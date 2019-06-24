6662716c39c426a6639e6c1e03e3cbb362f0e57d
Javier Jardón <javier.jardon@codethink.co.uk>
2019-06-23 19:24:16 -0700

    terraform: port to 0.12.x

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,13 @@
 //variable "ssh_key_name" {}
 
+terraform {
+  required_version = ">= 0.12.0"
+}
+
 # Configure aws provider
 provider "aws" {
-  region = "eu-west-1"
+  version = ">= 2.11"
+  region  = "eu-west-1"
 }
 
 ##################################################################
@@ -13,16 +18,16 @@ data "aws_vpc" "default" {
 }
 
 data "aws_subnet_ids" "all" {
-  vpc_id = "${data.aws_vpc.default.id}"
+  vpc_id = data.aws_vpc.default.id
 }
 
 module "security_group" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "2.10.0"
+  version = "3.0.1"
 
   name        = "example"
   description = "Security group for example usage with EC2 instance"
-  vpc_id      = "${data.aws_vpc.default.id}"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules       = ["all-tcp", "all-icmp"]
@@ -31,25 +36,26 @@ module "security_group" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "2.2.1"
+  version = "5.0.0"
 
   cluster_name              = "k8-cluster-${var.cluster_id}"
-  cluster_security_group_id = "${module.security_group.this_security_group_id}"
+  cluster_security_group_id = "module.security_group.this_security_group_id"
   cluster_version           = "1.11"
 
-  subnets = "${data.aws_subnet_ids.all.ids}"
+  subnets = data.aws_subnet_ids.all.ids
 
-  vpc_id = "${data.aws_vpc.default.id}"
+  vpc_id = data.aws_vpc.default.id
 
   worker_groups = [
     {
-      instance_type        = "${var.node_type}"
-      asg_desired_capacity = "${var.node_min_size}"
-      asg_max_size         = "${var.node_max_size}"
-      asg_min_size         = "${var.node_min_size}"
-      ebs_optimized        = "${var.ebs_optimized}"
+      instance_type        = var.node_type
+      asg_desired_capacity = var.node_min_size
+      asg_max_size         = var.node_max_size
+      asg_min_size         = var.node_min_size
+      ebs_optimized        = var.ebs_optimized
     },
   ]
 
-  worker_security_group_id = "${module.security_group.this_security_group_id}"
+  worker_security_group_id = module.security_group.this_security_group_id
 }
+
